f782b773e2fefa6e807682ff5681269bc129021e
grapefruit13 <liam.heo11@gmail.com>
2024-11-22 14:11:46 +0900

    feat: 템플릿 리스트, 인스턴스 리스트 필터 유틸함수 구현

--- src/utils/table.ts
+++ src/utils/table.ts
@@ -1,8 +1,15 @@
-import { BaseInstance } from "@/type/_field";
+import { INSTANCE_OPTION_LIST } from "@/constants/_filterOption";
+import {
+  InstanceCreatedAtField,
+  InstanceDescriptionField,
+  InstanceIdField,
+  InstanceNameField,
+} from "@/type/_field";
+import { InstanceListResponse } from "@/type/response/_instance";
 import { SimulationType } from "@/type/response/_simulation";
 
-export const filterDataList = (
-  dataList: BaseInstance[] | SimulationType[],
+export const filterSimulationList = (
+  dataList: SimulationType[],
   optionList: { label: string; value: string }[],
   keyword: string,
   filterType: string,
@@ -21,5 +28,34 @@ export const filterDataList = (
     if (!filterKey) return true;
 
     return String(value).toLowerCase().includes(keyword.toLowerCase());
+  });
+};
+
+export const filterInstances = (
+  instances: InstanceListResponse,
+  keyword: string,
+  filterType: string,
+) => {
+  return instances.filter((instance) => {
+    if (filterType === INSTANCE_OPTION_LIST[0].value) {
+      return instance[INSTANCE_OPTION_LIST[0].value as keyof InstanceIdField]
+        .toString()
+        .includes(keyword);
+    } else if (filterType === INSTANCE_OPTION_LIST[1].value) {
+      return instance[INSTANCE_OPTION_LIST[1].value as keyof InstanceNameField]
+        .toLowerCase()
+        .includes(keyword.toLowerCase());
+    } else if (filterType === INSTANCE_OPTION_LIST[2].value) {
+      return instance[
+        INSTANCE_OPTION_LIST[2].value as keyof InstanceDescriptionField
+      ]
+        .toLowerCase()
+        .includes(keyword.toLowerCase());
+    } else if (filterType === INSTANCE_OPTION_LIST[3].value) {
+      return instance[
+        INSTANCE_OPTION_LIST[3].value as keyof InstanceCreatedAtField
+      ].includes(keyword);
+    }
+    return true;
   });
 };
