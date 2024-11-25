7c0fcf1f0ce0c7dae8fb03e8d1d028437594513d
grapefruit13 <liam.heo11@gmail.com>
2024-11-25 10:31:12 +0900

    feat: 인스턴스 생성 모달 - isError 프롭 전달, 매직넘버 객체 참조하도록 수정

--- src/components/shared/InstanceCreateDialog/index.tsx
+++ src/components/shared/InstanceCreateDialog/index.tsx
@@ -15,24 +15,30 @@ type Props = {
   isOpen: boolean;
   simulationOptionList: Option[];
   errors: FieldErrors<CreateInstanceFormType>;
+  isError: {
+    templateId: boolean;
+    simulationId: boolean;
+  };
   onClose: () => void;
   register: UseFormRegister<CreateInstanceFormType>;
   handleSubmit: React.FormEventHandler<HTMLFormElement>;
-  setSelectedSimulationId: React.Dispatch<React.SetStateAction<string>>;
-  setSelectedTemplateId: React.Dispatch<React.SetStateAction<string>>;
+  setSelectedSimulationId: (id: string) => void;
+  setSelectedTemplateId: (id: string) => void;
 };
 
 const InstanceCreateDialog = ({
   isOpen,
   simulationOptionList,
   errors,
+  isError,
   onClose,
   register,
   handleSubmit,
   setSelectedSimulationId,
   setSelectedTemplateId,
 }: Props) => {
-  const [templateOptionList, setTemplateOptionList] = React.useState(
+  // TODO: 템플릿 목록 조회해서 초기값 넣기
+  const [templateOptionList, _] = React.useState(
     transformResponseToOptionList(
       MOCK_TEMPLATE_LIST,
       SCHEMA_NAME.TEMPLATE.ID,
@@ -87,20 +93,22 @@ const InstanceCreateDialog = ({
             label="시뮬레이션"
             placeholder="시뮬레이션을 선택하세요"
             onSelect={handleSimulationClick}
+            isError={isError.simulationId}
           />
           <SelectField
             optionList={templateOptionList}
             label="템플릿"
             placeholder="템플릿을 선택하세요"
             onSelect={handleTemplateClick}
+            isError={isError.templateId}
           />
           <InputField
             name={SCHEMA_NAME.INSTANCE.COUNT as keyof CreateInstanceFormType}
             label="개수"
             type="number"
             defaultValue={1}
-            min={1}
-            max={30}
+            min={INSTANCE_LENGTH_LIMIT.COUNT.MIN}
+            max={INSTANCE_LENGTH_LIMIT.COUNT.MAX}
             inputMode="numeric"
             placeholder="인스턴스 개수를 입력해주세요"
             maxLength={INSTANCE_LENGTH_LIMIT.COUNT.MAX}
