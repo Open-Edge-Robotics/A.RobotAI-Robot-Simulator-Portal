068e13ea698d9af7806906fc34657279d39b8498
grapefruit13 <liam.heo11@gmail.com>
2024-11-26 10:46:07 +0900

    fix: SimulationStatusField 타입 정의, SimulationListResponse에 적용

--- src/type/response/_simulation.ts
+++ src/type/response/_simulation.ts
@@ -1,10 +1,13 @@
-import { BaseSimulation, SimulationIdField } from "@/type/_field";
+import {
+  BaseSimulation,
+  SimulationIdField,
+  SimulationStatusField,
+} from "@/type/_field";
 
 // 타입 추후 추가 예정
-type SimulationStatusType = "RUNNING";
+// type SimulationStatusType = "RUNNING";
 
-export interface SimulationType extends BaseSimulation {
-  simulationStatus: SimulationStatusType;
+export interface SimulationType extends BaseSimulation, SimulationStatusField {
   [key: string]: string | number;
 }
 
