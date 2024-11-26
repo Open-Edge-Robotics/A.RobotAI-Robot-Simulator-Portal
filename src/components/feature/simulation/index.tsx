7d21d932e0df1e46fea13ee3c31f0d156fe38ada
grapefruit13 <liam.heo11@gmail.com>
2024-11-26 10:47:52 +0900

    feat: Simulation 페이지 - 시뮬레이션 목록 조회 api 연결, 목데이터 삭제

--- src/components/feature/simulation/index.tsx
+++ src/components/feature/simulation/index.tsx
@@ -7,47 +7,61 @@ import FilterGroup, {
   FilterGroupFormData,
 } from "@/components/shared/FilterGroup";
 import { SIMULATION_LIST_COLUMN_LIST } from "@/constants/_tableColumn";
-import { MOCK_SIMULATION_LIST } from "@/constants/mockData/instance";
 import { MENU_ITEMS } from "@/constants/_navbar";
 import {
   createSimulationShema,
   filterShema,
   SCHEMA_NAME,
 } from "@/schema/_schema";
-import { filterSimulationList } from "@/utils/table";
+import { filterSimulationList, formatSimulationCreatedAt } from "@/utils/table";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Typography } from "@mui/material";
 import SimulationListTable from "@/components/shared/SimulationListTable";
 import CreateButton from "@/components/shared/CreateButton";
 import SimulationCreateDialog from "@/components/shared/SimulationCreateDialog";
 import { CreateSimulationFormType } from "@/type/_simulation";
-import { SimulationType } from "@/type/response/_simulation";
+import {
+  SimulationListResponse,
+  SimulationType,
+} from "@/type/response/_simulation";
 import { SIMULATION_OPTION_LIST } from "@/constants/_filterOption";
 import { usePostSimulation } from "@/hooks/simulation/usePostSimulation";
 import { Result } from "@/type/response/_default";
 import { useToastStore } from "@/stores/useToastStore";
+import { useGetSimulationList } from "@/hooks/simulation/useGetSimulationList";
 
 const paginationModel = { page: 0, pageSize: 10 };
 
 const Simulation = () => {
+  const { data, isLoading } = useGetSimulationList();
+  const [simulationList, setSimulationList] =
+    React.useState<SimulationListResponse>([]);
   const [filterType, setFilterType] = React.useState<string>(
     SIMULATION_OPTION_LIST[0].value,
   );
-  // TODO: 렌더링 시 시뮬레이션 리스트 받아서 초기값으로 넣기
-  const [simulationList, setSimulationList] =
-    React.useState<SimulationType[]>(MOCK_SIMULATION_LIST);
   const [hasResult, setHasResult] = React.useState(true);
   const [isOpen, setIsOpen] = React.useState(false);
   const showToast = useToastStore((state) => state.showToast);
 
-  const handleSelectFilter = (value: string) => {
-    setFilterType(value);
-  };
+  React.useEffect(() => {
+    if (!isLoading && data) {
+      const formattedData = formatSimulationCreatedAt(data);
+      setSimulationList(formattedData);
+    }
+  }, [isLoading, data]);
+
+  React.useEffect(() => {
+    setHasResult(simulationList.length > 0);
+  }, [simulationList]);
+
+  const handleSelectFilter = (value: string) => setFilterType(value);
 
   const handleCloseDialog = () => {
     setIsOpen(false);
     dialogReset();
   };
+
+  const handleClickCreate = () => setIsOpen(true);
 
   // TODO: API 연결
   const handleExecute = (id: string) => {
@@ -60,8 +74,44 @@ const Simulation = () => {
     console.log("삭제 버튼 클릭", id);
   };
 
-  const handleClickCreate = () => {
-    setIsOpen(true);
+  // 검색 버튼 클릭 시 실행
+  const onFilterSubmit = (data: FilterGroupFormData) => {
+    const filteredList = filterSimulationList(
+      simulationList,
+      SIMULATION_OPTION_LIST,
+      data[SCHEMA_NAME.SEARCH_KEYWORD as keyof FilterGroupFormData],
+      filterType,
+    );
+    setSimulationList(
+      formatSimulationCreatedAt(filteredList) as SimulationType[],
+    );
+  };
+
+  // 검색어 없이 검색 버튼 클릭 시
+  const onFilterError = () => {
+    setHasResult(true);
+    if (!data) return;
+    setSimulationList(formatSimulationCreatedAt(data) || []);
+  };
+
+  const { mutate: simulationCreateMutate, error: simulationCreateError } =
+    usePostSimulation();
+  // 시뮬레이션 생성 모달에서 생성 버튼 클릭 시 실행
+  const onSimulationSubmit = (data: CreateSimulationFormType) => {
+    const { simulationName, simulationDescription } = data;
+    simulationCreateMutate(
+      {
+        simulationName,
+        simulationDescription,
+      },
+      {
+        onSuccess: ({ message }: Result<null>) => {
+          showToast(message, "success", 2000);
+          setIsOpen(false);
+        },
+      },
+    );
+    dialogReset();
   };
 
   const { register: filterRegister, handleSubmit: filterHandleSubmit } =
@@ -80,46 +130,13 @@ const Simulation = () => {
     mode: "onChange",
   });
 
-  const onFilterSubmit = (data: FilterGroupFormData) => {
-    const filteredList = filterSimulationList(
-      MOCK_SIMULATION_LIST,
-      SIMULATION_OPTION_LIST,
-      data[SCHEMA_NAME.SEARCH_KEYWORD as keyof FilterGroupFormData],
-      filterType,
+  if (isLoading) {
+    return (
+      <Typography variant="h6" className="text-sm font-normal text-gray-900">
+        Loading...
+      </Typography>
     );
-
-    if (filteredList.length <= 0) {
-      setHasResult(false);
-    } else {
-      setHasResult(true);
-      setSimulationList(filteredList as SimulationType[]);
-    }
-  };
-
-  // form 스키마 통과 못했을 때 -> 검색어 X
-  const onFilterError = () => {
-    setHasResult(true);
-    setSimulationList(MOCK_SIMULATION_LIST);
-  };
-
-  const { mutate: simulationCreateMutate, error: simulationCreateError } =
-    usePostSimulation();
-  // TODO: API 연결
-  const onSimulationSubmit = (data: CreateSimulationFormType) => {
-    simulationCreateMutate(
-      {
-        simulationName: data.simulationName,
-        simulationDescription: data.simulationDescription,
-      },
-      {
-        onSuccess: ({ message }: Result<null>) => {
-          showToast(message, "success", 2000);
-          setIsOpen(false);
-        },
-      },
-    );
-    dialogReset();
-  };
+  }
 
   return (
     <div className="flex flex-col gap-4">
