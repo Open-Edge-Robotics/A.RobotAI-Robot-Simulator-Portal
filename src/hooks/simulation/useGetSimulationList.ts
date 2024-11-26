6656e8fc16c873713c33c286b17dca73a606d88d
grapefruit13 <liam.heo11@gmail.com>
2024-11-26 11:25:39 +0900

    fix: usegetsimulatonlist usequery 타입 수정

--- src/hooks/simulation/useGetSimulationList.ts
+++ src/hooks/simulation/useGetSimulationList.ts
@@ -1,12 +1,14 @@
 import { simulation } from "@/api/simulation";
+import { Result } from "@/type/response/_default";
 import { SimulationListResponse } from "@/type/response/_simulation";
 import { useQuery } from "@tanstack/react-query";
+import { AxiosResponse } from "axios";
 
 /**
  * @description 시뮬레이션 목록 조회 쿼리
  */
 export const useGetSimulationList = () => {
-  return useQuery<SimulationListResponse>({
+  return useQuery<AxiosResponse<SimulationListResponse>>({
     queryKey: ["simulationList"],
     queryFn: () => simulation.getSimulationList(),
   });
