54a7b9fbffe669b2b094551279378262896af482
grapefruit13 <liam.heo11@gmail.com>
2024-11-26 10:43:58 +0900

    fix: useGetSimulationList - useQuery response 타입 지정

--- src/hooks/simulation/useGetSimulationList.ts
+++ src/hooks/simulation/useGetSimulationList.ts
@@ -1,13 +1,13 @@
 import { simulation } from "@/api/simulation";
+import { SimulationListResponse } from "@/type/response/_simulation";
 import { useQuery } from "@tanstack/react-query";
 
 /**
  * @description 시뮬레이션 목록 조회 쿼리
  */
-export const useGetSimulationList = (options?: { enabled?: boolean }) => {
-  return useQuery({
+export const useGetSimulationList = () => {
+  return useQuery<SimulationListResponse>({
     queryKey: ["simulationList"],
     queryFn: () => simulation.getSimulationList(),
-    ...options,
   });
 };
