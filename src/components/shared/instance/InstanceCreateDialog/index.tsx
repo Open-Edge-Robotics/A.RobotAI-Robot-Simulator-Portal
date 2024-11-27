00e57430d2843ce39331bb1af466e281767452d0
grapefruit13 <liam.heo11@gmail.com>
2024-11-27 10:39:57 +0900

    chore: shared/instance 으로 컴포넌트 경로 이동

--- src/components/shared/instance/InstanceCreateDialog/index.tsx
+++ src/components/shared/instance/InstanceCreateDialog/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { FieldErrors, UseFormRegister } from "react-hook-form";
 import { CreateInstanceFormType } from "@/type/_instance";
-import { Alert, Dialog, Typography } from "@mui/material";
+import { Alert, Dialog } from "@mui/material";
 import { INSTANCE_LENGTH_LIMIT, SCHEMA_NAME } from "@/schema/_schema";
 import InputField from "@/components/common/InputField";
-import CreateButton from "@/components/shared/CreateButton";
-import CancelButton from "@/components/shared/CancelButton/indext";
+import CreateButton from "@/components/shared/button/CreateButton";
+import CancelButton from "@/components/shared/button/CancelButton/indext";
 import SelectField from "@/components/common/SelectField";
 import { Option } from "@/components/shared/FilterGroup";
 import { transformResponseToOptionList } from "@/utils/option";
@@ -13,6 +13,10 @@ import { useGetTemplateList } from "@/hooks/template/useGetTemplateList";
 import { AxiosError } from "axios";
 import { Result } from "@/type/response/_default";
 import { API_ERROR_MESSAGE } from "@/constants/api/_errorMessage";
+import FormTitle from "@/components/common/FormTitle";
+import Form from "@/components/common/Form";
+import FormButtonContainer from "@/components/common/FormButtonContainer";
+import { LABEL, PLACE_HOLDER } from "@/constants/_form";
 
 type Props = {
   isOpen: boolean;
@@ -72,11 +76,8 @@ const InstanceCreateDialog = ({
         },
       }}
     >
-      <form
-        className="flex w-full scroll-m-0 flex-col items-center justify-center gap-8 p-8"
-        onSubmit={handleSubmit}
-      >
-        <Typography variant="h6">인스턴스 생성</Typography>
+      <Form onSubmit={handleSubmit}>
+        <FormTitle>인스턴스 생성</FormTitle>
         {error?.response?.status === 404 && (
           <Alert severity="error">
             {API_ERROR_MESSAGE.INSTANCE.CREATE[404]}
@@ -89,15 +90,14 @@ const InstanceCreateDialog = ({
         )}
         {!error && (
           <Alert severity="info">
-            {" "}
             {API_ERROR_MESSAGE.INSTANCE.CREATE.DEFAULT}
           </Alert>
         )}
         <div className="flex w-full flex-col gap-3">
           <InputField
             name={SCHEMA_NAME.INSTANCE.NAME as keyof CreateInstanceFormType}
-            label="이름"
-            placeholder="인스턴스 이름을 입력해주세요"
+            label={LABEL.NAME}
+            placeholder={PLACE_HOLDER.INSTANCE_CREATE.NAME}
             maxLength={INSTANCE_LENGTH_LIMIT.NAME.MAX}
             register={register}
             errors={errors}
@@ -106,43 +106,43 @@ const InstanceCreateDialog = ({
             name={
               SCHEMA_NAME.INSTANCE.DESCRIPTION as keyof CreateInstanceFormType
             }
-            label="설명"
-            placeholder="인스턴스 설명을 입력해주세요"
+            label={LABEL.DESCRIPTION}
+            placeholder={PLACE_HOLDER.INSTANCE_CREATE.DESCRIPTION}
             maxLength={INSTANCE_LENGTH_LIMIT.DESCRIPTION.MAX}
             register={register}
             errors={errors}
           />
           <SelectField
             optionList={simulationOptionList}
-            label="시뮬레이션"
-            placeholder="시뮬레이션을 선택하세요"
+            label={LABEL.SIMULATION}
+            placeholder={PLACE_HOLDER.INSTANCE_CREATE.SIMULATION}
             onSelect={handleSimulationClick}
             isError={isError.simulationId}
           />
           <SelectField
             optionList={templateOptionList}
-            label="템플릿"
-            placeholder="템플릿을 선택하세요"
+            label={LABEL.TEMPLATE}
+            placeholder={PLACE_HOLDER.INSTANCE_CREATE.TEMPLATE}
             onSelect={handleTemplateClick}
             isError={isError.templateId}
           />
           <InputField
             name={SCHEMA_NAME.INSTANCE.COUNT as keyof CreateInstanceFormType}
-            label="개수"
+            label={LABEL.COUNT}
             type="number"
             defaultValue={1}
             min={INSTANCE_LENGTH_LIMIT.COUNT.MIN}
             max={INSTANCE_LENGTH_LIMIT.COUNT.MAX}
             inputMode="numeric"
-            placeholder="인스턴스 개수를 입력해주세요"
+            placeholder={PLACE_HOLDER.INSTANCE_CREATE.COUNT}
             register={register}
           />
         </div>
-        <div className="ml-auto flex gap-2">
+        <FormButtonContainer>
           <CancelButton onClick={onClose} />
           <CreateButton type="submit" />
-        </div>
-      </form>
+        </FormButtonContainer>
+      </Form>
     </Dialog>
   );
 };
