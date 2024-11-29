4614776b24068bb04405f5f8b9fd9e9ff9e0392f
grapefruit13 <liam.heo11@gmail.com>
2024-11-29 11:37:52 +0900

    refactor: SimulationFilter - disabled일 시 SelectValueText 수정

--- src/components/shared/simulation/SimulationFilter/index.tsx
+++ src/components/shared/simulation/SimulationFilter/index.tsx
@@ -14,29 +14,40 @@ type Props = {
 };
 
 const SimulationFilter = ({ optionList, onSelect }: Props) => {
+  const isOptionsEmpty = optionList.length === 0;
+
   return (
     <Select
       className="relative w-80 text-center align-middle"
       onSelect={onSelect}
     >
-      <SelectTrigger className="w-full rounded-[4px] bg-white p-2">
+      <SelectTrigger
+        className="w-full rounded-[4px] bg-white p-2"
+        disabled={isOptionsEmpty}
+      >
         <SelectValueText
           className="text-sm"
-          placeholder={"시뮬레이션을 선택하세요"}
+          placeholder={
+            isOptionsEmpty
+              ? "생성된 시뮬레이션이 없습니다"
+              : "조회할 시뮬레이션을 선택하세요"
+          }
         />
       </SelectTrigger>
-      <SelectGroup className="top-[42px] max-h-40 w-full rounded-[4px] bg-white p-2 shadow-md">
-        {optionList.map((item) => (
-          <SelectItem
-            key={item.value}
-            value={item.value}
-            label={item.label}
-            className="w-full rounded-[4px] px-2 py-1 text-sm"
-          >
-            {item.label}
-          </SelectItem>
-        ))}
-      </SelectGroup>
+      {!isOptionsEmpty && (
+        <SelectGroup className="top-[42px] max-h-40 w-full rounded-[4px] bg-white p-2 shadow-md">
+          {optionList.map((item) => (
+            <SelectItem
+              key={item.value}
+              value={item.value}
+              label={item.label}
+              className="w-full rounded-[4px] px-2 py-1 text-sm"
+            >
+              {item.label}
+            </SelectItem>
+          ))}
+        </SelectGroup>
+      )}
       <SelectIcon
         color="#000000"
         className="absolute right-2 top-[10px] z-20"
