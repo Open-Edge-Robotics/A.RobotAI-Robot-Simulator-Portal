5b16941e8e3368e8b38ea3fb455cd7dd882ac747
grapefruit13 <liam.heo11@google.com>
2024-12-03 10:58:49 +0900

    feat: CORS 우회를 위한 reverse proxy 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "standalone",
+  rewrites: async () => {
+    return {
+      beforeFiles: [
+        {
+          source: "/api/:path*",
+          destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+        },
+      ],
+    };
+  },
+};
 
 export default nextConfig;
