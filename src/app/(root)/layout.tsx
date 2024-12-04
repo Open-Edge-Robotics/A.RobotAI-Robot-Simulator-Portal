4c7dc96dd0894fe0ef65d617592056a64cec9ecb
grapefruit13 <liam.heo11@google.com>
2024-12-04 11:22:06 +0900

    style: RootLayout 스타일 수정

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -3,9 +3,11 @@ import Navbar from "@/components/common/Navbar";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex min-h-screen">
+    <div className="flex h-screen">
       <Navbar />
-      <main className="bg-black flex-grow p-8 pt-6">{children}</main>
+      <main className="bg-black w-full flex-grow overflow-x-hidden p-8 pt-6">
+        {children}
+      </main>
     </div>
   );
 };
