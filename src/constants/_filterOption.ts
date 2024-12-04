1da1873a1fec913efd42dcf52c6ed72035e56dbc
grapefruit13 <liam.heo11@google.com>
2024-12-04 10:55:23 +0900

    fix: 인스턴스, 시뮬레이션 옵션 리스트 수정

--- src/constants/_filterOption.ts
+++ src/constants/_filterOption.ts
@@ -1,14 +1,14 @@
 const SIMULATION_OPTION_LIST = [
-  { label: "ID", value: "simulationId" },
   { label: "이름", value: "simulationName" },
+  { label: "ID", value: "simulationId" },
   { label: "설명", value: "simulationDescription" },
   { label: "생성일", value: "simulationCreatedAt" },
   { label: "상태", value: "simulationStatus" },
 ];
 
 const INSTANCE_OPTION_LIST = [
-  { label: "ID", value: "instanceId" },
   { label: "이름", value: "instanceName" },
+  { label: "ID", value: "instanceId" },
   { label: "설명", value: "instanceDescription" },
   { label: "생성일", value: "instanceCreatedAt" },
   { label: "파드 이름", value: "podName" },
