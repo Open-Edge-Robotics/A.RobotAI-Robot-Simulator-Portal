abd60e6080b891db3626afc0c0514154858e1ad8
grapefruit13 <liam.heo11@google.com>
2024-12-04 15:36:36 +0900

    fix: 삭제 버튼 disabled 스타일 수정

--- src/components/shared/button/DeleteButton/index.tsx
+++ src/components/shared/button/DeleteButton/index.tsx
@@ -1,10 +1,10 @@
 import { Button, ButtonBaseProps } from "@mui/material";
-import React from "react";
 
 const DeleteButton = ({ onClick, disabled }: ButtonBaseProps) => {
   return (
     <Button
       variant="contained"
+      className="disabled:bg-gray-400 disabled:text-white"
       color="error"
       onClick={onClick}
       disabled={disabled}
