787b79b770ab158eb00bcd077221418355b792be
grapefruit13 <liam.heo11@google.com>
2024-12-05 15:25:57 +0900

    fix: 프록시 해제, 인스턴스 실행 액션 응답 오래걸리는 이슈

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
-  rewrites: async () => {
-    return {
-      beforeFiles: [
-        {
-          source: "/api/:path*",
-          destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
-        },
-      ],
-    };
-  },
+  // output: "standalone",
+  // rewrites: async () => {
+  //   return {
+  //     beforeFiles: [
+  //       {
+  //         source: "/api/:path*",
+  //         destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+  //       },
+  //     ],
+  //   };
+  // },
 };
 
 export default nextConfig;
