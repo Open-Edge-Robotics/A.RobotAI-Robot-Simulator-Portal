700d80d221b844e7b1f25059323e6a88e9a653dc
grapefruit13 <liam.heo11@google.com>
2024-12-06 14:13:03 +0900

    feat: 시뮬레이션 실행/중지 api 함수 정의

--- src/api/simulation/index.ts
+++ src/api/simulation/index.ts
@@ -1,15 +1,20 @@
 import { apiDelete, apiGet, apiPost } from "@/lib/axios";
 import { BASE_API } from "@/constants/api/_apiPath";
-import { PostSimulationRequest } from "@/type/request/_simulation";
+import {
+  PostSimulationActionRequest,
+  PostSimulationRequest,
+} from "@/type/request/_simulation";
 import { Result } from "@/type/response/_default";
 import {
   SimulaionDeleteResponse,
   SimulationListResponse,
+  SimulationPostActionResponse,
   SimulationPostResponse,
 } from "@/type/response/_simulation";
 import { SimulationIdField } from "@/type/_field";
 
 const simulationURL = BASE_API.SIMULATION;
+const actionURL = BASE_API.ACTION;
 
 /**
  * @description 시뮬레이션 목록 조회
@@ -39,6 +44,26 @@ const postSimulation = async ({
 };
 
 /**
+ * @description 시뮬레이션 실행/중지
+ * @param {PostSimulationActionRequest} - 시뮬레이션 ID, 액션 (실행 혹은 중지)
+ */
+const postSimulationAction = async ({
+  simulationId,
+  action,
+}: PostSimulationActionRequest): Promise<
+  Result<SimulationPostActionResponse>
+> => {
+  const response = await apiPost<Result<SimulationPostActionResponse>>(
+    `${simulationURL}/${actionURL}`,
+    {
+      simulationId,
+      action,
+    },
+  );
+  return response.data;
+};
+
+/**
  * @description 시뮬레이션 삭제
  * @param {SimulationIdField} - 시뮬레이션 ID
  * @returns {SimulaionDeleteResponse}
@@ -55,5 +80,6 @@ const deleteSimulation = async ({
 export const simulation = {
   getSimulationList,
   postSimulation,
+  postSimulationAction,
   deleteSimulation,
 };
