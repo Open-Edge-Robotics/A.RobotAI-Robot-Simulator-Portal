f520d7c4bfbaca5a2ad169d722289b827f7ac38d
grapefruit13 <liam.heo11@google.com>
2024-12-10 11:38:43 +0900

    fix: 인스턴스 목록 삭제 쿼리 request 타입 수정

--- src/hooks/instance/useDeleteInstanceList.ts
+++ src/hooks/instance/useDeleteInstanceList.ts
@@ -7,7 +7,7 @@ import { Result } from "@/type/response/_default";
  * @description 인스턴스 목록 삭제 쿼리
  */
 export const useDeleteInstanceList = () => {
-  return useMutation<Result<null>[], AxiosError<Result<null>>, string[]>({
-    mutationFn: (request: string[]) => instance.deleteInstanceList(request),
+  return useMutation<Result<null>[], AxiosError<Result<null>>, number[]>({
+    mutationFn: (request: number[]) => instance.deleteInstanceList(request),
   });
 };
